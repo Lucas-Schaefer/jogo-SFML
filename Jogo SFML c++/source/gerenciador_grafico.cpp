f2ce5a253ae99dbe16b2bd1d79d67780c05f6016
Matheus-E-Valente <mvalente@alunos.utfpr.edu.br>
2024-12-06 22:40:35 -0300

    gerenciador_grafico.cpp

---
+++ Jogo SFML c++/source/gerenciador_grafico.cpp
@@ -0,0 +1,5 @@
+#include "gerenciador_grafico.hpp"
+using namespace std;
+Gerenciador_Grafico::Gerenciador_Grafico(){}
+Gerenciador_Grafico::~Gerenciador_Grafico(){}
+Gerenciador_Grafico::desenharEnte(Ente* pE){}
