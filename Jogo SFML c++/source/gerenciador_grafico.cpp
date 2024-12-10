4446b32b1ae7f641e743654cc50e5cfcc5fc9439
Lucas-Schaefer <lucascontischaefer@gmail.com>
2024-12-09 21:37:51 -0300

    elemento.hpp
    #pragma once
    #include<iostream>
    using namespace std;
    class Elemento
    {
    private:
    	Elemento*<TE>pProx;
    	TE* pInfo;
    public:
    	Elemento();
    	~Elemento();
    	void incluir(TE* p);
    	void setPRoximo(Elemento* <TE> pE);
    	Elemento* <TE> getPRoximo const();
    };

--- Jogo SFML c++/source/gerenciador_grafico.cpp
+++ Jogo SFML c++/source/gerenciador_grafico.cpp
@@ -1,5 +1,21 @@
+#include<iostream>
+#include<SFML/Graphics.hpp>
 #include "gerenciador_grafico.hpp"
+#define TELAX 1600.0f
+#define TELAY 900.0f
 using namespace std;
-Gerenciador_Grafico::Gerenciador_Grafico(){}
-Gerenciador_Grafico::~Gerenciador_Grafico(){}
-Gerenciador_Grafico::desenharEnte(Ente* pE){}
+using namespace sf;
+namespace Gerenciadores {
+	Gerenciador_grafico* Gerenciador_grafico::pGerenciador = nullptr;
+	Gerenciador_grafico* Gerenciador_grafico::getGerenciador()
+	{
+		if (pGerenciador == nullptr)
+		{
+			return new Gerenciador_grafico;
+		}
+		else
+		{
+			return pGerenciador;
+		}
+	}
+}
