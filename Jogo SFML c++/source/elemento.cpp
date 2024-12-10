7b9274e2e9dad907ddfc63220b7457ebd78b551b
Lucas-Schaefer <lucascontischaefer@gmail.com>
2024-12-09 21:44:10 -0300

    Add files via upload

---
+++ Jogo SFML c++/source/elemento.cpp
@@ -0,0 +1,10 @@
+#pragma once
+#include<iostream>
+#include"elemento.hpp"
+using namespace std;
+	Elemento::Elemento() {}
+	Elemento::~Elemento() {}
+	Elemento::void incluir(TE* p) {}
+	Elemento::void setPRoximo(Elemento* <TE> pE) {}
+	Elemento::Elemento* <TE> getPRoximo const() {}
+};
