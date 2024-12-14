1566559810572febc9a4a266b8caaaae512d2962
Matheus-E-Valente <mvalente@alunos.utfpr.edu.br>
2024-12-14 16:07:18 -0300

    personagem..cpp

---
+++ Jogo SFML c++/source/personagem.cpp
@@ -0,0 +1,8 @@
+#include "personagem.hpp"
+
+Personagem::Personagem();
+Personagem::~Personagem();
+Personagem::salvarDataBuffer();
+virtual void Personagem::mover();
+virtual void Personagem::executar();
+    }
