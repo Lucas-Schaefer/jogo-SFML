679203a3c4e80cb76186a3330dea7b33d8dac629
Matheus-E-Valente <mvalente@alunos.utfpr.edu.br>
2024-12-14 14:31:39 -0300

    entidade.cpp

---
+++ Jogo SFML c++/source/entidade.cpp
@@ -0,0 +1,7 @@
+#include "entdade.hpp"
+
+Entidade::Entidade() {}
+Entidade::~Entidade() {}
+virtual void Entidade::executar() {}
+virtual void Entidade::salvar() {}
+void Entidade::salvarDataBuffer() {}
